9ca8b91b19817cc5108387f4d62bff0b3c277555
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-07-08 14:15:02 +0100

    Update s3_bucket.tf

--- modules/s3_bucket/s3_bucket.tf
+++ modules/s3_bucket/s3_bucket.tf
@@ -1,8 +1,3 @@
-
-#############
-# s3 bucket
-#############
-
 resource "aws_s3_bucket" "s3_bucket" {
   bucket              = var.s3_bucket_bucket
   bucket_prefix       = var.s3_bucket_bucket_prefix
