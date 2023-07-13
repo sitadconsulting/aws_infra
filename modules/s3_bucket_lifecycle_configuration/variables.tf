1c2c399a0e8be710ed638b405b55a2824cea812e
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-07-13 14:12:49 +0100

    Update variables.tf

--- modules/s3_bucket_lifecycle_configuration/variables.tf
+++ modules/s3_bucket_lifecycle_configuration/variables.tf
@@ -14,12 +14,12 @@ variable "s3_bucket_lifecycle_configuration_rule" {
     status = string
     abort_incomplete_multipart_upload = optional(list(object({
       days_after_initiation       = optional(number)      
-    })))
+    })), [])
     expiration                        = optional(list(object({
       date                         = optional(string)
       days                         = optional(number)
       expired_object_delete_marker = optional(bool)
-    })))
+    })), [])
     filter                            = optional(list(object({
       object_size_greater_than     = optional(number)
       object_size_less_than        = optional(number)
@@ -29,26 +29,26 @@ variable "s3_bucket_lifecycle_configuration_rule" {
         object_size_less_than      = optional(number)
         prefix                     = optional(string)
         tags                       = optional(map(string))
-      })))
+      })), [])
       tag                             = optional(list(object({
         key   = string
         value = string
-      }))) 
-    })))
+      })), []) 
+    })), [])
     noncurrent_version_expiration     = optional(list(object({
       newer_noncurrent_versions    = optional(number)
       noncurrent_days              = optional(number)
-    })))
+    })), [])
     noncurrent_version_transition     = optional(list(object({
       newer_noncurrent_versions    = optional(number)
       noncurrent_days              = optional(number)
       storage_class                = string
-    })))
+    })), [])
     transition                        = optional(list(object({
       date                         = optional(string)
       days                         = optional(number)
       storage_class                = string
-    })))
+    })), [])
   }))
   default     = []
 }
