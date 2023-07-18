639ea647244375f40e3eef6878daf4084e2a4204
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-07-18 14:59:35 +0100

    Update variable.tf

--- modules/vpc_endpoint_policy/variable.tf
+++ modules/vpc_endpoint_policy/variable.tf
@@ -5,5 +5,5 @@ variable "vpc_endpoint_policy_policy" {
 variable "vpc_endpoint_policy_vpc_endpoint_id" {
   description = "A policy to attach to the endpoint that controls access to the service. Defaults to full access"
   type        = string
-  default     = ""
+  default     = null
 }
