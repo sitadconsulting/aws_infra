c8068b89e689d1c92afd93522aeb6393069a1f26
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-08-14 09:50:58 +0100

    Update variables.tf

--- modules/glue_job/variables.tf
+++ modules/glue_job/variables.tf
@@ -69,7 +69,7 @@ variable "glue_job_timeout" {
 variable "glue_job_worker_type" {
   description = "The type of predefined worker that is allocated when a job runs. Accepts a value of Standard, G.1X, or G.2X"
   type        = string
-  default     = "G.1X"
+  default     = null
 }
 variable "glue_job_command" {
   description = "The command of the job"
