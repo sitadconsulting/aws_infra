6f207d3495b09f896bd3995f5cf63f704fe73334
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-08-25 12:59:29 +0100

    Update variables.tf

--- modules/glue_partition/variables.tf
+++ modules/glue_partition/variables.tf
@@ -1,7 +1,7 @@
 variable "glue_partition_catalog_id" {
   description = "ID of the Glue Catalog and database to create the table in. If omitted, this defaults to the AWS Account ID plus the database name"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "glue_partition_database_name" {
   description = "Name of the metadata database where the table metadata resides. For Hive compatibility, this must be all lowercase"
