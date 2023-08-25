39c9a9c4aeb2cb1f9375dd24c55358955b7796a9
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-08-25 13:37:54 +0100

    Update variables.tf

--- modules/autoscaling_schedule/variables.tf
+++ modules/autoscaling_schedule/variables.tf
@@ -10,7 +10,7 @@ variable "autoscaling_schedule_desired_capacity" {
 variable "autoscaling_schedule_end_time" {
   description = "The date and time for the recurring schedule to end, in UTC with the format \"YYYY-MM-DDThh:mm:ssZ\" (e.g. \"2021-06-01T00:00:00Z\")"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "autoscaling_schedule_max_size" {
   description = "The maximum size of the Auto Scaling group. Set to -1 if you don't want to change the maximum size at the scheduled time. Defaults to 0"
@@ -25,7 +25,7 @@ variable "autoscaling_schedule_min_size" {
 variable "autoscaling_schedule_recurrence" {
   description = "The recurring schedule for this action specified using the Unix cron syntax format"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "autoscaling_schedule_scheduled_action_name" {
   description = "The name of this scaling action"
@@ -34,10 +34,10 @@ variable "autoscaling_schedule_scheduled_action_name" {
 variable "autoscaling_schedule_start_time" {
   description = "The date and time for the recurring schedule to start, in UTC with the format \"YYYY-MM-DDThh:mm:ssZ\" (e.g. \"2021-06-01T00:00:00Z\")"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "autoscaling_schedule_time_zone" {
   description = "Specifies the time zone for a cron expression. Valid values are the canonical names of the IANA time zones (such as Etc/GMT+9 or Pacific/Tahiti)"
   type        = string
-  default     = ""
+  default     = null
 }
