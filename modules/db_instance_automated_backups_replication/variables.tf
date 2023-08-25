4b8f095afbdfc03de2ea64b4af69c4d10fef87a2
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-08-25 11:48:55 +0100

    Update variables.tf

--- modules/db_instance_automated_backups_replication/variables.tf
+++ modules/db_instance_automated_backups_replication/variables.tf
@@ -1,12 +1,12 @@
 variable "db_instance_automated_backups_replication_kms_key_id" {
   description = "The AWS KMS key identifier for encryption of the replicated automated backups. The KMS key ID is the Amazon Resource Name (ARN) for the KMS encryption key in the destination AWS Region, for example, arn:aws:kms:us-east-1:123456789012:key/AKIAIOSFODNN7EXAMPLE"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "db_instance_automated_backups_replication_pre_signed_url" {
   description = "A URL that contains a Signature Version 4 signed request for the StartDBInstanceAutomatedBackupsReplication action to be called in the AWS Region of the source DB instance"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "db_instance_automated_backups_replication_retention_period" {
   description = "The retention period for the replicated automated backups, defaults to 7"
