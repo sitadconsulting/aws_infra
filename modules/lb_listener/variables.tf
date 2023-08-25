1500a4fdfabd147c8fab1132fee58712bae54cac
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-08-25 13:41:07 +0100

    Update variables.tf

--- modules/lb_listener/variables.tf
+++ modules/lb_listener/variables.tf
@@ -5,12 +5,12 @@ variable "lb_listener_load_balancer_arn" {
 variable "lb_lb_listener_alpn_policy" {
   description = "Name of the Application-Layer Protocol Negotiation (ALPN) policy. Can be set if protocol is TLS. Valid values are HTTP1Only, HTTP2Only, HTTP2Optional, HTTP2Preferred, and None"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "lb_lb_listener_certificate_arn" {
   description = "ARN of the default SSL server certificate. Exactly one certificate is required if the protocol is HTTPS"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "lb_lb_listener_port" {
   description = "Port on which the load balancer is listening. Not valid for Gateway Load Balancers"
@@ -20,12 +20,12 @@ variable "lb_lb_listener_port" {
 variable "lb_lb_listener_protocol" {
   description = " Protocol for connections from clients to the load balancer. For Application Load Balancers, valid values are HTTP and HTTPS, with a default of HTTP. For Network Load Balancers, valid values are TCP, TLS, UDP, and TCP_UDP. Not valid to use UDP or TCP_UDP if dual-stack mode is enabled. Not valid for Gateway Load Balancers"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "lb_lb_listener_ssl_policy" {
   description = "Name of the SSL Policy for the listener. Required if protocol is HTTPS or TLS"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "lb_lb_listener_tags" {
   description = "A map of tags to assign to the resource"
