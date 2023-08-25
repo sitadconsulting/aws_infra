b28d4de20dcdbcaf3b2d7d18a632a5303a1782e7
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-08-25 11:53:55 +0100

    Update variables.tf

--- modules/db_snapshot_copy/variables.tf
+++ modules/db_snapshot_copy/variables.tf
@@ -6,22 +6,22 @@ variable "db_snapshot_copy_copy_tags" {
 variable "db_snapshot_copy_destination_region" {
   description = "The Destination region to place snapshot copy "
   type        = string
-  default     = ""
+  default     = null
 }
 variable "db_snapshot_copy_kms_key_id" {
   description = "KMS key ID"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "db_snapshot_copy_option_group_name" {
   description = "The name of an option group to associate with the copy of the snapshot"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "db_snapshot_copy_presigned_url" {
   description = "The URL that contains a Signature Version 4 signed request"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "db_snapshot_copy_source_db_snapshot_identifier" {
   description = "Snapshot identifier of the source snapshot"
@@ -30,7 +30,7 @@ variable "db_snapshot_copy_source_db_snapshot_identifier" {
 variable "db_snapshot_copy_target_custom_availability_zone" {
   description = "The external custom Availability Zone"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "db_snapshot_copy_target_db_snapshot_identifier" {
   description = "The Identifier for the snapshot"
