b23b3564fe711d8aef6ec3febdc0de4a7fef2479
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-08-28 18:17:39 +0100

    Update output.tf

--- modules/cloudwatch_log_group/output.tf
+++ modules/cloudwatch_log_group/output.tf
@@ -2,3 +2,7 @@ output "cloudwatch_log_group_arn" {
   description = "The ARNs of this cloudwatch log group"
   value       = element(concat(aws_cloudwatch_log_group.cloudwatch_log_group.*.arn, [""]), 0)
 }
+output "cloudwatch_log_group_name" {
+  description = "Cloudwatch log group name"
+  value       = element(concat(split(":",aws_cloudwatch_log_group.cloudwatch_log_group.arn), [""]), 6)
+}
