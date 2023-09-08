927d4d6e6d509562f34e2db605ca2688abdabbbc
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-09-08 17:33:44 +0100

    Update output.tf

--- modules/db_subnet_group/output.tf
+++ modules/db_subnet_group/output.tf
@@ -6,9 +6,9 @@ output "db_subnet_group_arn" {
   description = "ARN of the db subnet group"
   value       = element(concat(aws_db_subnet_group.db_subnet_group.*.arn, [""]), 0)
 }
-output "db_subnet_group_supported_network_type" {
+output "db_subnet_group_supported_network_types" {
   description = "The network type of the db subnet group"
-  value       = element(concat(aws_db_subnet_group.db_subnet_group.*.supported_network_type, [""]), 0)
+  value       = element(concat(aws_db_subnet_group.db_subnet_group.*.supported_network_types, [""]), 0)
 }
 output "db_subnet_group_vpc_id" {
   description = "Provides the VPC ID of the DB subnet group"
