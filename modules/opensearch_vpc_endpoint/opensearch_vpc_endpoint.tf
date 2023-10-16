59e57eb247e1fc59c123714e31a10945e07114ca
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-10-16 13:08:08 +0100

    Update opensearch_vpc_endpoint.tf

--- modules/opensearch_vpc_endpoint/opensearch_vpc_endpoint.tf
+++ modules/opensearch_vpc_endpoint/opensearch_vpc_endpoint.tf
@@ -1,1 +1,11 @@
+resource "aws_opensearch_vpc_endpoint" "opensearch_vpc_endpoint" {
+  domain_arn = var.opensearch_vpc_endpoint_domain_arn
 
+  dynamic "vpc_options" {
+    for_each = var.opensearch_vpc_endpoint_vpc_options
+      content {
+        security_group_ids = vpc_options.value["security_group_ids"]
+        subnet_ids         = vpc_options.value["subnet_ids"]
+      }
+  }
+}
