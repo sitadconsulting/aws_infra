e8f02f6dd023398178f6395e6263cc2c63cf6ff8
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-10-16 13:26:05 +0100

    Update output.tf

--- modules/opensearch_vpc_endpoint/output.tf
+++ modules/opensearch_vpc_endpoint/output.tf
@@ -1,1 +1,8 @@
-
+output "opensearch_vpc_endpoint_id" {
+  description = "The unique identifier of the endpoint"
+  value       = element(concat(aws_opensearch_vpc_endpoint.opensearch_vpc_endpoint.*.id, [""]), 0)
+}
+output "opensearch_vpc_endpoint_endpoint" {
+  description = "The connection endpoint ID for connecting to the domain"
+  value       = element(concat(aws_opensearch_vpc_endpoint.opensearch_vpc_endpoint.*.endpoint, [""]), 0)
+}
