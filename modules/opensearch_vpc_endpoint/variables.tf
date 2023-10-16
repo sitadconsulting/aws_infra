9bbad18cfb1eb0548aeece28c9930e583b28aa49
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2023-10-16 13:18:34 +0100

    Update variables.tf

--- modules/opensearch_vpc_endpoint/variables.tf
+++ modules/opensearch_vpc_endpoint/variables.tf
@@ -1,1 +1,12 @@
-
+variable "opensearch_vpc_endpoint_domain_arn" {
+  description = "Specifies the Amazon Resource Name (ARN) of the domain to create the endpoint for"
+  type        = string
+}
+variable "opensearch_vpc_endpoint_vpc_options" {
+  description = "Configuration Options to specify the subnets and security groups for the endpoint"
+  type        = list(object({
+    security_group_ids = optional(list(string))
+    subnet_ids         = list(string)
+  }))
+  default     = []
+}
