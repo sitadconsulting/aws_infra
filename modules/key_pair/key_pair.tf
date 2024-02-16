21afb577b14719705bf0160a19286e64cb46022d
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2024-02-16 18:22:09 +0000

    Update key_pair.tf

--- modules/key_pair/key_pair.tf
+++ modules/key_pair/key_pair.tf
@@ -2,7 +2,7 @@ resource "aws_key_pair" "key_pair" {
   key_name        = var.key_pair_key_name
   key_name_prefix = var.key_pair_key_name_prefix
   public_key      = var.key_pair_public_key 
-  tags =          = var.key_pair_tags
+  tags            = var.key_pair_tags
 }
 
 
