132e6f1629e87dd0fc614c9a1a93ca4e654af4da
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2024-02-16 18:21:38 +0000

    Update key_pair.tf

--- modules/key_pair/key_pair.tf
+++ modules/key_pair/key_pair.tf
@@ -1,20 +1,8 @@
 resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_pair_key_name
-  public_key = var.key_pair_public_key 
-
-  tags = merge(
-    {
-      "Name" = format(
-        "%s-key_pair-%s-%s",
-        var.name,
-        var.key_pair_key_name,
-        var.environment,
-      )
-    },
-    var.tags,
-    var.key_pair_tags,
-  )
-
+  key_name        = var.key_pair_key_name
+  key_name_prefix = var.key_pair_key_name_prefix
+  public_key      = var.key_pair_public_key 
+  tags =          = var.key_pair_tags
 }
 
 
