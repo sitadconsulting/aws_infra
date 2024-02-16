82e8bd60583415f7b979b2107ac6584ee24b7751
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2024-02-16 18:28:34 +0000

    Update variables.tf

--- modules/key_pair/variables.tf
+++ modules/key_pair/variables.tf
@@ -1,30 +1,21 @@
-variable "name" {
-  description = "Name to be used on all the resources as identifier"
-  type        = string
-  default     = "sitadinfra"
-}
-variable "environment" {
-  description = "The name of the environment"
-  type        = string
-  default     = "DEV"
-}
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
-variable "key_pair_tags" {
-  description = "Additional tags for this key"
-  type        = map(string)
-  default     = {}
-}
 variable "key_pair_key_name" {
   description = "The name of the key pair to create"
   type        = string
   default     = ""
+}
+variable "key_pair_key_name_prefix" {
+  description = "Creates a unique name beginning with the specified prefix. Conflicts with key_name. If neither key_name nor key_name_prefix is provided, Terraform will create a unique key name using the prefix terraform-"
+  type        = string
+  default     = null
 }
 variable "key_pair_public_key" {
   description = "The public key of the key pair to create"
   type        = string
   default     = ""
 }
+variable "key_pair_tags" {
+  description = "Additional tags for this key"
+  type        = map(string)
+  default     = {}
+}
+
