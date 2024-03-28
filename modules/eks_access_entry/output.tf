02648628b541c2c3562fcdf6a9e4bab1c1343803
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2024-03-28 11:58:11 +0000

    Update output.tf

--- modules/eks_access_entry/output.tf
+++ modules/eks_access_entry/output.tf
@@ -1,4 +1,4 @@
 output "access_entry_arn" {
   description = "ARN of the Access Entry"
-  value       = element(concat(aws_eks_access_entry.eks_access_entry.*.arn, [""]), 0)
+  value       = element(concat(aws_eks_access_entry.eks_access_entry.*.access_entry_arn, [""]), 0)
 }
