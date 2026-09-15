ad5deba8dfdaef1f23156182b25d931530428c71
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-15 12:56:15 +0100

    Update variables.tf

--- modules/apigatewayv2_model/variables.tf
+++ modules/apigatewayv2_model/variables.tf
@@ -9,7 +9,7 @@ variable "apigatewayv2_model_content_type" {
 variable "apigatewayv2_model_description" {
   description = "Description of the model. Must be between 1 and 128 characters in length"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "apigatewayv2_model_name" {
   description = "Name of the model. Must be alphanumeric. Must be between 1 and 128 characters in length"
