63fa3305526d583e150d2fddc4467a2f707667ac
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-15 12:54:32 +0100

    Update variables.tf

--- modules/apigatewayv2_deployment/variables.tf
+++ modules/apigatewayv2_deployment/variables.tf
@@ -5,7 +5,7 @@ variable "apigatewayv2_deployment_api_id" {
 variable "apigatewayv2_deployment_description" {
   description = "Description for the deployment resource. Must be less than or equal to 1024 characters in length"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "apigatewayv2_deployment_triggers" {
   description = "Map of arbitrary keys and values that, when changed, will trigger a redeployment. To force a redeployment without changing these keys/values, use the terraform taint command"
