09a44047adb54d55aef1f559248de84f4a7e5bc0
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-15 12:53:14 +0100

    Update variables.tf

--- modules/apigatewayv2_api_mapping/variables.tf
+++ modules/apigatewayv2_api_mapping/variables.tf
@@ -5,7 +5,7 @@ variable "apigatewayv2_api_mapping_api_id" {
 variable "apigatewayv2_api_mapping_api_mapping_key" {
   description = "The API mapping key. Refer to REST API, HTTP API or WebSocket API"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "apigatewayv2_api_mapping_domain_name" {
   description = "Domain name. Use the aws_apigatewayv2_domain_name resource to configure a domain name"
