610021a4c09eed5eb68f9f5ecd9cf0db13dd43cc
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:17:48 +0100

    Update variables.tf

--- modules/redshift_snapshot_schedule/variables.tf
+++ modules/redshift_snapshot_schedule/variables.tf
@@ -1,12 +1,12 @@
 variable "redshift_snapshot_schedule_definitions" {
   description = "The definition of the snapshot schedule. The definition is made up of schedule expressions, for example cron(30 12 *) or rate(12 hours)"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "redshift_snapshot_schedule_description" {
   description = "The description of the snapshot schedule"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "redshift_snapshot_schedule_force_destroy" {
   description = "Whether to destroy all associated clusters with this snapshot schedule on deletion. Must be enabled and applied before attempting deletion"
