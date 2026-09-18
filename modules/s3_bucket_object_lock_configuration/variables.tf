ec9f800618cbe1224cf3ac6151c63a2ffbb75bee
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:30:29 +0100

    Update variables.tf

--- modules/s3_bucket_object_lock_configuration/variables.tf
+++ modules/s3_bucket_object_lock_configuration/variables.tf
@@ -5,17 +5,17 @@ variable "s3_bucket_object_lock_configuration_bucket" {
 variable "s3_bucket_object_lock_configuration_expected_bucket_owner" {
   description = "Account ID of the expected bucket owner"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "s3_bucket_object_lock_configuration_object_lock_enabled" {
   description = "Indicates whether this bucket has an Object Lock configuration enabled. Defaults to Enabled. Valid values: Enabled"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "s3_bucket_object_lock_configuration_token" {
   description = "Token to allow Object Lock to be enabled for an existing bucket. You must contact AWS support for the bucket's "Object Lock token". The token is generated in the back-end when versioning is enabled on a bucket"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "s3_bucket_object_lock_configuration_rule" {
   description = "Object Lock rule configuration for the specified object"
