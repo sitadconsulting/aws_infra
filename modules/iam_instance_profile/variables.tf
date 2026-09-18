9d86ee1985862785fe06b7eaf900c75adcfa8c0c
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 11:48:20 +0100

    Update variables.tf

--- modules/iam_instance_profile/variables.tf
+++ modules/iam_instance_profile/variables.tf
@@ -1,7 +1,7 @@
 variable "iam_instance_profile_name" {
   description = "Name of the instance profile. If omitted, Terraform will assign a random, unique name. Conflicts with name_prefix"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "iam_instance_profile_name_prefix" {
   description = "Creates a unique name beginning with the specified prefix. Conflicts with name"
@@ -16,7 +16,7 @@ variable "iam_instance_profile_path" {
 variable "iam_instance_profile_role" {
   description = "Name of the role to add to the profile"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "iam_instance_profile_tags" {
   description = "Map of resource tags for the IAM Instance Profile"
