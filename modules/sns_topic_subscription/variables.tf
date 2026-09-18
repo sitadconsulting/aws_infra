0a6279f7bdb212e7bf961f3c52258af97c585e12
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:43:18 +0100

    Update variables.tf

--- modules/sns_topic_subscription/variables.tf
+++ modules/sns_topic_subscription/variables.tf
@@ -6,7 +6,7 @@ variable "sns_topic_subscription_confirmation_timeout_in_minutes" {
 variable "sns_topic_subscription_delivery_policy" {
   description = "JSON String with the delivery policy (retries, backoff, etc.) that will be used in the subscription - this only applies to HTTP/S subscriptions"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_topic_subscription_endpoint" {
   description = "Endpoint to send data to. The contents vary with the protocol"
@@ -20,12 +20,12 @@ variable "sns_topic_subscription_endpoint_auto_confirms" {
 variable "sns_topic_subscription_filter_policy" {
   description = "JSON String with the filter policy that will be used in the subscription to filter messages seen by the target resource"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_topic_subscription_filter_policy_scope" {
   description = "Whether the filter_policy applies to MessageAttributes (default) or MessageBody "
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_topic_subscription_protocol" {
   description = "Protocol to use. Valid values are: sqs, sms, lambda, firehose, and application. Protocols email, email-json, http and https are also valid but partially supported"
@@ -39,15 +39,15 @@ variable "sns_topic_subscription_raw_message_delivery" {
 variable "sns_topic_subscription_redrive_policy" {
   description = "JSON String with the redrive policy that will be used in the subscription"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_topic_subscription_subscription_role_arn" {
   description = "ARN of the IAM role to publish to Kinesis Data Firehose delivery stream"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_topic_subscription_topic_arn" {
   description = "ARN of the SNS topic to subscribe to"
   type        = string
-  default     = ""
+  default     = null
 }
