f423b1a92c14a6413364a4cbeb47121aee1cfd40
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:14:09 +0100

    Update variables.tf

--- modules/rds_cluster_parameter_group/variables.tf
+++ modules/rds_cluster_parameter_group/variables.tf
@@ -1,7 +1,7 @@
 variable "rds_cluster_parameter_group_description" {
   description = "The description of the DB cluster parameter group. Defaults to \"Managed by Terraform\""
   type        = string
-  default     = ""
+  default     = null
 }
 variable "rds_cluster_parameter_group_family" {
   description = "The family of the DB cluster parameter group"
@@ -10,7 +10,7 @@ variable "rds_cluster_parameter_group_family" {
 variable "rds_cluster_parameter_group_name" {
   description = "The name of the DB cluster parameter group. If omitted, Terraform will assign a random, unique name"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "rds_cluster_parameter_group_name_prefix" {
   description = "Creates a unique name beginning with the specified prefix. Conflicts with name"
