93c7b7b1d0f0450e5c6faa75f8c85e0d01ca6501
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:11:15 +0100

    Update variables.tf

--- modules/quicksight_iam_policy_assignment/variables.tf
+++ modules/quicksight_iam_policy_assignment/variables.tf
@@ -9,7 +9,7 @@ variable "quicksight_iam_policy_assignment_assignment_status" {
 variable "quicksight_iam_policy_assignment_aws_account_id" {
   description = "AWS account ID"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "quicksight_iam_policy_assignment_namespace" {
   description = "Namespace that contains the assignment. Defaults to default"
@@ -19,7 +19,7 @@ variable "quicksight_iam_policy_assignment_namespace" {
 variable "quicksight_iam_policy_assignment_policy_arn" {
   description = "ARN of the IAM policy to apply to the Amazon QuickSight users and groups specified in this assignment"
   type        = string 
-  default     = ""
+  default     = null
 }
 variable "quicksight_iam_policy_assignment_identities" {
   description = "Amazon QuickSight users, groups, or both to assign the policy to"
