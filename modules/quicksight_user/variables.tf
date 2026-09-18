bb94c8fce01811ac51f1699a994d5927522849ac
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:13:05 +0100

    Update variables.tf

--- modules/quicksight_user/variables.tf
+++ modules/quicksight_user/variables.tf
@@ -1,7 +1,7 @@
 variable "quicksight_user_aws_account_id" {
   description = "The ID for the AWS account that the user is in. Currently, you use the ID for the AWS account that contains your Amazon QuickSight account"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "quicksight_user_email" {
   description = "The email address of the user that you want to register"
@@ -10,7 +10,7 @@ variable "quicksight_user_email" {
 variable "quicksight_user_iam_arn" {
   description = "The ARN of the IAM user or role that you are registering with Amazon QuickSight"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "quicksight_user_identity_type" {
   description = "Amazon QuickSight supports several ways of managing the identity of users. This parameter accepts either IAM or QUICKSIGHT. If IAM is specified, the iam_arn must also be specified"
@@ -19,17 +19,17 @@ variable "quicksight_user_identity_type" {
 variable "quicksight_user_namespace" {
   description = "The Amazon Quicksight namespace to create the user in. Defaults to default"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "quicksight_user_session_name" {
   description = "The name of the IAM session to use when assuming roles that can embed QuickSight dashboards. Only valid for registering users using an assumed IAM role. Additionally, if registering multiple users using the same IAM role, each user needs to have a unique session name"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "quicksight_user_user_name" {
   description = "The Amazon QuickSight user name that you want to create for the user you are registering. Only valid for registering a user with identity_type set to QUICKSIGHT"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "quicksight_user_user_role" {
   description = "The Amazon QuickSight role of the user. The user role can be one of the following: READER, AUTHOR, or ADMIN"
