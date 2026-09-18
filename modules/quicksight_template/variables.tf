2286ba76df5731530218b1458f6d8129664b290c
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:12:24 +0100

    Update variables.tf

--- modules/quicksight_template/variables.tf
+++ modules/quicksight_template/variables.tf
@@ -1,7 +1,7 @@
 variable "quicksight_template_aws_account_id" {
   description = "AWS account ID"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "quicksight_template_name" {
   description = "Display name for the template"
