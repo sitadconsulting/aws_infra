7ec114218503aa73c5901f1c302ff28ea8738c8e
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 11:45:30 +0100

    Update variables.tf

--- modules/glue_user_defined_function/variables.tf
+++ modules/glue_user_defined_function/variables.tf
@@ -1,7 +1,7 @@
 variable "glue_user_defined_function_catalog_id" {
   description = "ID of the Glue Catalog to create the function in. If omitted, this defaults to the AWS Account ID"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "glue_user_defined_function_class_name" {
   description = "The Java class that contains the function code"
