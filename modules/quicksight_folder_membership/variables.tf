82854c9eb1763408b7a25f4fde366aa85fdbd6c8
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:10:26 +0100

    Update variables.tf

--- modules/quicksight_folder_membership/variables.tf
+++ modules/quicksight_folder_membership/variables.tf
@@ -1,7 +1,7 @@
 variable "quicksight_folder_membership_aws_account_id" {
   description = "AWS account ID"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "quicksight_folder_membership_folder_id" {
   description = "Identifier for the folder"
