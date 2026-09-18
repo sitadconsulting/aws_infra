4377e424e9909e985be074d81e4340bf9c960f4a
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 11:58:33 +0100

    Update variables.tf

--- modules/kubernetes_persistent_volume_claim/variables.tf
+++ modules/kubernetes_persistent_volume_claim/variables.tf
@@ -1,12 +1,12 @@
 variable "persistent_volume_claim_metadata_name" {
   description = "Name of the Persistent Volume Claim, must be unique "
   type        = string
-  default     = ""
+  default     = null
 }
 variable "persistent_volume_claim_metadata_namespace" {
   description = "Namespace defines the space within which name of the Persistent Volume Claim must be unique "
   type        = string
-  default     = ""
+  default     = null
 }
 variable "persistent_volume_claim_spec_access_modes" {
   description = "A set of the desired access modes the volume should have"
@@ -16,12 +16,12 @@ variable "persistent_volume_claim_spec_access_modes" {
 variable "persistent_volume_claim_spec_volume_name" {
   description = "The binding reference to the PersistentVolume backing this claim"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "persistent_volume_claim_spec_storage_class_name" {
   description = "Name of the storage class requested by the claim"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "persistent_volume_claim_spec_resources" {
   description = "A list of the minimum resources the volume should have"
