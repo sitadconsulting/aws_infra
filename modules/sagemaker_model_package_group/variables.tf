3b566c0f93945530a7385744c0405e784f677287
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:36:41 +0100

    Update variables.tf

--- modules/sagemaker_model_package_group/variables.tf
+++ modules/sagemaker_model_package_group/variables.tf
@@ -5,7 +5,7 @@ variable "sagemaker_model_package_group_model_package_group_name" {
 variable "sagemaker_model_package_group_model_package_group_description" {
   description = "A description for the model group"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sagemaker_model_package_group_tags" {
   description = " A map of tags to assign to the resource. If configured with a provider default_tags configuration block present, tags with matching keys will overwrite those defined at the provider-level"
