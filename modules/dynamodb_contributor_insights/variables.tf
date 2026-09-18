2988f860c6f0078097ddb9e28462ed5dc066f066
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 11:27:05 +0100

    Update variables.tf

--- modules/dynamodb_contributor_insights/variables.tf
+++ modules/dynamodb_contributor_insights/variables.tf
@@ -1,7 +1,7 @@
 variable "dynamodb_contributor_insights_index_name" {
   description = "The global secondary index name"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "dynamodb_contributor_insights_table_name" {
   description = "The name of the table to enable contributor insights"
