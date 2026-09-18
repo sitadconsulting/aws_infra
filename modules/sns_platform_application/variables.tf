a46a9af309eddb93795945468807bf0bd058d40f
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:40:29 +0100

    Update variables.tf

--- modules/sns_platform_application/variables.tf
+++ modules/sns_platform_application/variables.tf
@@ -1,22 +1,22 @@
 variable "sns_platform_application_apple_platform_team_id" {
   description = "The identifier that's assigned to your Apple developer account team. Must be 10 alphanumeric characters"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_platform_application_apple_platform_bundle_id" {
   description = "The bundle identifier that's assigned to your iOS app. May only include alphanumeric characters, hyphens (-), and periods (.)."
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_platform_application_event_delivery_failure_topic_arn" {
   description = "The ARN of the SNS Topic triggered when a delivery to any of the platform endpoints associated with your platform application encounters a permanent failure"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_platform_application_event_endpoint_created_topic_arn" {
   description = "The ARN of the SNS Topic triggered when a new platform endpoint is added to your platform application"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_platform_application_event_endpoint_deleted_topic_arn" {
   description = "The ARN of the SNS Topic triggered when an existing platform endpoint is deleted from your platform application"
@@ -26,12 +26,12 @@ variable "sns_platform_application_event_endpoint_deleted_topic_arn" {
 variable "sns_platform_application_event_endpoint_updated_topic_arn" {
   description = "The ARN of the SNS Topic triggered when an existing platform endpoint is changed from your platform application"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_platform_application_failure_feedback_role_arn" {
   description = "The IAM role ARN permitted to receive failure feedback for this application and give SNS write access to use CloudWatch logs on your behalf"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_platform_application_name" {
   description = "The friendly name for the SNS platform application"
@@ -48,12 +48,12 @@ variable "sns_platform_application_platform_credential" {
 variable "sns_platform_application_platform_principal" {
   description = "Application Platform principal"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_platform_application_success_feedback_role_arn" {
   description = "The IAM role ARN permitted to receive success feedback for this application and give SNS write access to use CloudWatch logs on your behalf"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "sns_platform_application_success_feedback_sample_rate" {
   description = "The sample rate percentage (0-100) of successfully delivered messages"
