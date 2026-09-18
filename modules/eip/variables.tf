05e2f43f74c260ed63de298761194e201e814d40
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 11:38:18 +0100

    Update variables.tf

--- modules/eip/variables.tf
+++ modules/eip/variables.tf
@@ -1,17 +1,17 @@
 variable "eip_address" {
   description = "IP address from an EC2 BYOIP pool. This option is only available for VPC EIPs"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "eip_associate_with_private_ip" {
   description = "User-specified primary or secondary private IP address to associate with the Elastic IP address. If no private IP address is specified, the Elastic IP address is associated with the primary private IP address"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "eip_customer_owned_ipv4_pool" {
   description = "ID of a customer-owned address pool"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "eip_domain" {
   description = "Indicates if this EIP is for use in VPC (vpc)"
@@ -21,22 +21,22 @@ variable "eip_domain" {
 variable "eip_instance" {
   description = "EC2 instance ID"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "eip_network_border_group" {
   description = "Location from which the IP address is advertised. Use this parameter to limit the address to this location"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "eip_network_interface" {
   description = "Network interface ID to associate with"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "eip_public_ipv4_pool" {
   description = "EC2 IPv4 address pool identifier or amazon. This option is only available for VPC EIPs"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "eip_tags" {
   description = "Map of tags to assign to the resource"
