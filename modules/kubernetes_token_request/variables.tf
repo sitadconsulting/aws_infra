2cef7cc6369894511b91ff664e9cc316c6764a20
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:00:44 +0100

    Update variables.tf

--- modules/kubernetes_token_request/variables.tf
+++ modules/kubernetes_token_request/variables.tf
@@ -11,12 +11,12 @@ variable "token_request_metadata_labels" {
 variable "token_request_metadata_name" {
   description = "Name of the token, must be unique"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "token_request_metadata_namespace" {
   description = "Namespace defines the space within which name of the tolen must be unique"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "token_request_spec" {
   description = "Spec holds information about the request being evaluated"
