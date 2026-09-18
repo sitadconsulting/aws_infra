57c850bfee4a251521edb308e0092df107ff315a
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:03:46 +0100

    Update variables.tf

--- modules/launch_configuration/variables.tf
+++ modules/launch_configuration/variables.tf
@@ -16,7 +16,7 @@ variable "launch_configuration_enable_monitoring" {
 variable "launch_configuration_iam_instance_profile" {
   description = "The name attribute of the IAM instance profile to associate with launched instances"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "launch_configuration_image_id" {
   description = "The EC2 image ID to launch"
@@ -29,12 +29,12 @@ variable "launch_configuration_instance_type" {
 variable "launch_configuration_key_name" {
   description = "The key name that should be used for the instance"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "launch_configuration_name" {
   description = "The name of the launch configuration. If you leave this blank, Terraform will auto-generate a unique name. Conflicts with name_prefix"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "launch_configuration_name_prefix" {
   description = "Creates a unique name beginning with the specified prefix. Conflicts with name"
@@ -44,7 +44,7 @@ variable "launch_configuration_name_prefix" {
 variable "launch_configuration_placement_tenancy" {
   description = "The tenancy of the instance. Valid values are default or dedicated"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "launch_configuration_security_groups" {
   description = "A list of associated security group IDS"
@@ -54,17 +54,17 @@ variable "launch_configuration_security_groups" {
 variable "launch_configuration_spot_price" {
   description = "The maximum price to use for reserving spot instances"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "launch_configuration_user_data" {
   description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "launch_configuration_user_data_base64" {
   description = "Can be used instead of user_data to pass base64-encoded binary data directly. Use this instead of user_data whenever the value is not a valid UTF-8 string. For example, gzip-encoded user data must be base64-encoded and passed via this argument to avoid corruption"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "launch_configuration_ebs_block_device" {
   description = "Additional EBS block devices to attach to the instance"
