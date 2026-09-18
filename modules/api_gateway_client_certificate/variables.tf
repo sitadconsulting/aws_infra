ef9d17514941e85867c1bcedae585d486affaba2
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 11:09:43 +0100

    Update variables.tf

--- modules/api_gateway_client_certificate/variables.tf
+++ modules/api_gateway_client_certificate/variables.tf
@@ -1,7 +1,7 @@
 variable "api_gateway_client_certificate_description" {
   description = "Description of the client certificate"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "api_gateway_client_certificate_tags" {
   description = "Key-value map of resource tags. If configured with a provider default_tags configuration block present, tags with matching keys will overwrite those defined at the provider-level"
