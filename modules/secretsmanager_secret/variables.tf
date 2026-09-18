22bdf6207b1f62296cfa2c366184d66fae75ae20
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:38:43 +0100

    Update variables.tf

--- modules/secretsmanager_secret/variables.tf
+++ modules/secretsmanager_secret/variables.tf
@@ -1,22 +1,22 @@
 variable "secret_description" {
   description = "Description of the secret"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "secret_kms_key_id" {
   description = "ARN or Id of the AWS KMS key to be used to encrypt the secret values in the versions stored in this secret. If you don't specify this value, Secrets Manager defaults to using the AWS account's default KMS key (the one named aws/secretsmanager)"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "secret_name" {
   description = "Friendly name of the new secret"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "secret_policy" {
   description = "Valid JSON document representing a resource policy"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "secret_recovery_window_in_days" {
   description = "Number of days that AWS Secrets Manager waits before it can delete the secret. This value can be 0 to force deletion without recovery or range from 7 to 30 days. The default value is 30"
