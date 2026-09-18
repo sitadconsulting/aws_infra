1c3b125156963b5e525c7b982e240a8252667858
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:17:30 +0100

    Update variables.tf

--- modules/redshift_snapshot_copy_grant/variables.tf
+++ modules/redshift_snapshot_copy_grant/variables.tf
@@ -1,7 +1,7 @@
 variable "redshift_snapshot_copy_grant_kms_key_id" {
   description = "The unique identifier for the customer master key (CMK) that the grant applies to. Specify the key ID or the ARN of the CMK"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "redshift_snapshot_copy_grant_snapshot_copy_grant_name" {
   description = "A friendly name for identifying the grant"
