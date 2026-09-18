38c34a134438d3d2e5a7563173fbe5e70d15db80
sitadconsulting <138239755+sitadconsulting@users.noreply.github.com>
2026-09-18 12:14:58 +0100

    Update variables.tf

--- modules/rds_global_cluster/variables.tf
+++ modules/rds_global_cluster/variables.tf
@@ -1,7 +1,7 @@
 variable "rds_global_cluster_database_name" {
   description = "Name for an automatically created database on cluster creation"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "rds_global_cluster_deletion_protection" {
   description = "Wether the Global Cluster should have deletion protection enabled. The database can't be deleted when this value is set to true. The default is false"
@@ -16,7 +16,7 @@ variable "rds_global_cluster_engine" {
 variable "rds_global_cluster_engine_version" {
   description = "Engine version of the Aurora global database. The engine, engine_version, and instance_class (on the aws_rds_cluster_instance) must together support global databases"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "rds_global_cluster_force_destroy" {
   description = "Enable to remove DB Cluster members from Global Cluster on destroy. Required with source_db_cluster_identifier "
@@ -30,7 +30,7 @@ variable "rds_global_cluster_global_cluster_identifier" {
 variable "rds_global_cluster_source_db_cluster_identifier" {
   description = "ARN to use as the primary DB Cluster of the Global Cluster on creation. Terraform cannot perform drift detection of this value"
   type        = string
-  default     = ""
+  default     = null
 }
 variable "rds_global_cluster_storage_encrypted" {
   description = "Specifies whether the DB cluster is encrypted. The default is false unless source_db_cluster_identifier is specified and encrypted"
